02cb28835086d3dd043f6719ada769d77992a603
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-17 16:19:20 +0900

    Add docstrings

--- src/expanda/extension.py
+++ src/expanda/extension.py
@@ -3,6 +3,20 @@ from typing import Dict
 
 
 class Extension(object):
+    r"""Wrapper class of extensions.
+
+    Every extensions would define their informations into `__extension__`
+    variable. For using extensions, this wrapper class provides simple
+    interface to handle them. It summarizes attributes and helps executing
+    them.
+
+    Caution:
+        This class dynamically import the given `module_name`. Please make sure
+        that the extension module can be imported in current environment.
+
+    Arguments:
+        module_name (str): Module name of extension.
+    """
     def __init__(self, module_name: str):
         # Import extension module.
         module = importlib.import_module(module_name)
@@ -31,6 +45,19 @@ class Extension(object):
 
     def call(self, input_file: str, output_file: str, temporary: str,
              raw_args: Dict[str, str]):
+        r"""Call main code of the extension.
+
+        Note:
+            Every extensions have their own parameter requirements in
+            `__extension__` variable. This function automatically cast
+            the type of each string-formatted raw argument.
+
+        Arguments:
+            input_file (str): Input file path.
+            output_file (str): Output file path.
+            temporary (str): Temporary directory where the extension would use.
+            raw_args (dict): String-formatted raw arguments for the extension.
+        """
         args = {}
         for name, req in self.arg_reqs.items():
             if 'default' not in req and name not in raw_args:
