5ee18009ac9ca22d15c4755cbe3b155765f5ab85
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-17 15:50:42 +0900

    Initialize the script

---
+++ src/expanda/ext/namu.py
@@ -0,0 +1,10 @@
+
+
+__extension__ = {
+    'name': 'namuwiki extractor',
+    'version': '1.0',
+    'description': 'extract namuwiki json file.',
+    'author': 'expanda',
+    'main': None,
+    'arguments': {}
+}
