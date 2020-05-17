7a17ce2f5a9602cdb5618a7b3d3b637a51c59b76
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-17 15:49:24 +0900

    Update main codes

--- src/expanda/__main__.py
+++ src/expanda/__main__.py
@@ -1,27 +1,40 @@
 import re
+import os
+import shutil
 import argparse
 from configparser import ConfigParser
 from .extension import Extension
+from .shuffling import shuffle
+from .tokenization import train_tokenizer, tokenize_corpus
 
 
-def _show_extension_details(module_name):
+def _show_extension_details(module_name: str):
     # Show the details of extension.
     ext = Extension(module_name)
     print(f'Extension [{ext.module_name}]\n'
-          f'Name: {ext.ext_name}\n'
-          f'Version: {ext.version}\n'
-          f'Description: {ext.description}\n'
-          f'Author: {ext.author}')
+          f'Name             : {ext.ext_name}\n'
+          f'Version          : {ext.version}\n'
+          f'Description      : {ext.description}\n'
+          f'Author           : {ext.author}')
+
+    # Print required parameters.
+    print('Parameters')
+    for name, opt in ext.arg_reqs.items():
+        print('    {:12s} : {:6s}'.format(name, opt['type'].__name__), end='')
+        if 'default' in opt:
+            print(f' (default: {opt["default"]})')
+        else:
+            print('')
 
 
-def _show_required_extension_list(config_file):
+def _show_required_extension_list(config_file: str):
     # Read config file.
     config = ConfigParser()
     config.read(config_file)
 
     # Parse `input-files` option to get required extension list.
     input_files = config['DEFAULT'].get('input-files')
-    exts = set(re.match(r'--(.*?)\s', line).groups()[0]
+    exts = set(re.match(r'--(.*?)\s', line).group(0)
                for line in input_files.splitlines(False)
                if line)
 
@@ -31,6 +44,67 @@ def _show_required_extension_list(config_file):
     for ext in exts:
         version = Extension(ext).version
         print(f'{ext[:25]:25s}{version[:10]:10s}')
+
+
+def _build_corpus(config_file: str):
+    # Read config file.
+    config = ConfigParser()
+    config.read(config_file)
+
+    input_files = config['build'].get('input-files')
+    input_files = [re.match(r'--(.*?)\s+(.*)', line.strip()).groups()
+                   for line in input_files.splitlines(False)
+                   if line]
+
+    # Read arguments from configuration file.
+    temporary = config['build'].get('temporary-path', './tmp')
+    vocab = config['build'].get('output-vocab', 'vocab.txt')
+    corpus = config['build'].get('output-corpus', 'corpus.txt')
+    raw_corpus = config['build'].get('output-raw-corpus', 'corpus.raw.txt')
+
+    subset_size = config['tokenize'].getint('subset-size', fallback=10000000)
+    vocab_size = config['tokenize'].getint('vocab-size', fallback=8000)
+    unk_token = config['tokenize'].get('unk-token', '<unk>')
+
+    control_tokens = config['tokenize'].get('control-tokens', '')
+    control_tokens = [token.strip()
+                      for token in control_tokens.splitlines()
+                      if token]
+
+    # Create temporary directory if not exists.
+    try:
+        os.makedirs(temporary)
+    except FileExistsError:
+        pass
+
+    # Extract raw corpus file to plain sentences.
+    for ext, input_file in input_files:
+        print(f'[*] execute extension [{ext}] for [{input_file}]')
+        Extension(ext).call(input_file,
+                            os.path.join(temporary, input_file),
+                            temporary,
+                            dict(config.items(ext)))
+
+    # Gather the extracted plain text.
+    print('[*] merge extracted texts...')
+    with open(os.path.join(temporary, 'integrated'), 'wb') as dst:
+        for _, input_file in input_files:
+            with open(os.path.join(temporary, input_file), 'rb') as src:
+                shutil.copyfileobj(src, dst)
+
+    # Shuffle the text.
+    print('[*] start shuffling merged corpus.')
+    shuffle(os.path.join(temporary, 'integrated'), raw_corpus, temporary)
+
+    # Train subword tokenizer and tokenize the corpus.
+    train_tokenizer(raw_corpus, vocab, temporary, subset_size, vocab_size,
+                    unk_token, control_tokens)
+    tokenize_corpus(raw_corpus, corpus, vocab, unk_token, control_tokens)
+
+    # Remove temporary directory.
+    shutil.rmtree(temporary)
+
+    print('[*] finish building corpus.')
 
 
 if __name__ == '__main__':
@@ -65,4 +139,4 @@ if __name__ == '__main__':
     elif args.command == 'show':
         _show_extension_details(args.extension)
     elif args.command == 'build':
-        pass
+        _build_corpus(args.config)
