2c4cd601286595928f50026f503110fecfd42f8c
affjljoo3581 <affjljoo3581@gmail.com>
2020-05-23 18:56:32 +0900

    Add docstring

--- src/expanda/utils.py
+++ src/expanda/utils.py
@@ -11,9 +11,10 @@ def random_filename(parent: str) -> str:
     r"""Generate random filename consists of 16 characters.
 
     Arguments:
+        parent (str): Parent directory for file.
 
     Returns:
-
+        Randomly generated file name.
     """
     return os.path.join(parent, ''.join(random.choices(_CANDIDATES, k=16)))
 
@@ -22,8 +23,10 @@ def random_filenames(parent: str, n: int) -> List[str]:
     r"""Generate multiple random filenames.
 
     Arguments:
+        parent (str): Parent directory for files.
+        n (int): Total number of filenames.
 
     Returns:
-
+        Randomly generated file names.
     """
     return [random_filename(parent) for _ in range(n)]
